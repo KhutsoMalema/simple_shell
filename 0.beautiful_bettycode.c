ff1949358ead48a7b087a3d8ce8a9d31f168959e
KhutsoMalema <malemakhutso6@gmail.com>
2023-12-17 07:26:40 -0800

    0.beautiful_bettycode.c

---
+++ 0.beautiful_bettycode.c
@@ -0,0 +1,25 @@
+/*
+** File: beautiful_bettycode.c
+** Author: Khutso Malema
+** Description: A beautiful code that passes the Betty checks.
+*/
+
+#include <stdio.h>
+
+/**
+* print_hello - Prints a beautiful "Hello, World!" message.
+*/
+void print_hello(void)
+{
+printf("Hello, World!\n");
+}
+
+/**
+* main - The main function that prints a beautiful message.
+* Return: 0 on success.
+*/
+int main(void)
+{
+print_hello();
+return (0);
+}
